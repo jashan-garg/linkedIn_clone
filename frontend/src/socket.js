9413c4b1f8697535b930ac94d8786c3ad618c5fa
Jashan Garg <133480057+jashan-garg@users.noreply.github.com>
2025-12-22 17:45:14 +0530

    Change socket connection URL to production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:8000', {
+export const socket = io('https://linkedin-backend-3b3o.onrender.com', {
     withCredentials: true,
     autoConnect: true,
 });
