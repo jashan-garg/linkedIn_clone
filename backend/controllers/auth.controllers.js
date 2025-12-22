1350638129ad349c0134acc781e888832cbbf668
Jashan Garg <133480057+jashan-garg@users.noreply.github.com>
2025-12-22 18:18:53 +0530

    Update cookie settings for auth responses

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -29,9 +29,9 @@ export const signUp = async (req, res) => {
         let token = await getToken(user._id);
         res.cookie('token', token, {
             httpOnly: true,
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: 'strict',
-            secure: process.env.NODE_ENVIRONMENT == 'production',
         });
 
         console.log('Signed Up Successfully');
@@ -57,9 +57,9 @@ export const logIn = async (req, res) => {
         let token = await getToken(user._id);
         res.cookie('token', token, {
             httpOnly: true,
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: 'strict',
-            secure: process.env.NODE_ENVIRONMENT == 'production',
         });
 
         console.log('Logged In Successfully');
@@ -75,8 +75,8 @@ export const logOut = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            sameSite: 'strict',
-            secure: process.env.NODE_ENVIRONMENT === 'production',
+            secure: true,
+            sameSite: 'none',
         });
 
         return res.status(200).json({
